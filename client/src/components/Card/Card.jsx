d22f21d4b81d1dd42bf6feda02dbb11dcdafed52
Nahu Castilla <nahuelcastilla22@gmail.com>
2023-05-04 22:25:27 +0200

    form comenzado

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,13 +2,9 @@ import style from "./Card.module.css"
 const Card = (props) => {
     return (
         <div className={style.card}>
+            <img src={props.imagen} alt=""/>
             <p>Name:{props.nombre}</p>
-            <p>Imagen:{props.imagen}</p>
             <p>Continente:{props.continente}</p>
-            <p>Capital:{props.capital}</p>
-            <p>Subregion:{props.subregion}</p>
-            <p>Area:{props.area}</p>
-            <p>Poblacion:{props.poblacion}</p>
         </div>
     )
 }
--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -1,10 +1,56 @@
-const Form = () =>  {
+import { useState } from "react";
 
+const Form = () =>  {
+    const [form, setForm] = useState({
+        nombre: "aaaaaaaa   ",
+        dificultad:"",
+        duracion:"",
+        temporada:"",
+        paises: []
+    });
+
+const changeHandler = (event) => {
+    const property = event.target.name ;
+    const value = event.target.value;
+    console.log({[property]:value})
+    setForm({...form,[property]: value})
+}
     return (
-        <h1>
-            Hola soy el Form
-        </h1>
+        <form>
+            <div>
+                <label>Nombre de la actividad: </label>
+                <input type="text" value={form.nombre} onChange={changeHandler} name="nombre"></input>
+            </div>
+
+            <div>
+                <label>Dificultad: </label>
+                <input type="number" min="1" max="5" multiple/>
+            </div>
+
+            <div>
+                <label>Duracion: </label>
+                <input type="time" name="hora" step="3600"/>
+            </div>
+ 
+            <div>
+                <label>Temporada: </label>
+                <select name="enum">
+                    <option value="valor1">Opción 1</option>
+                    <option value="valor2">Opción 2</option>
+                    <option value="valor3">Opción 3</option>
+</select>
+            </div>  
+        </form>
     )
 }
 
 export default Form;
+
+/*
+Nombre.
+Dificultad.
+Duración.
+Temporada.
+Posibilidad de seleccionar/agregar varios países en simultáneo.
+Botón para crear la actividad turística.
+*/
