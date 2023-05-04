35d20965954fbe8a3669cd0b568c6c525755f8a0
Nahu Castilla <nahuelcastilla22@gmail.com>
2023-05-04 23:11:42 +0200

    form sin submitear

--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -1,25 +1,52 @@
 import { useState } from "react";
 
 const Form = () =>  {
-    const [form, setForm] = useState({
-        nombre: "aaaaaaaa   ",
+   
+const [form, setForm] = useState({
+        nombre: "",
         dificultad:"",
         duracion:"",
         temporada:"",
         paises: []
     });
 
-const changeHandler = (event) => {
-    const property = event.target.name ;
+const [errors, setErrors] = useState({
+        nombre: "",
+        dificultad:"",
+        duracion:"",
+        temporada:"",
+        paises: []
+})
+
+const validate = (form) => {
+    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(form.nombre)){
+        console.log("Si pasa el regex --->", form.nombre);
+        setErrors({...errors, nombre:""})
+    } else {
+        console.log("No pasa una shit el regex");
+        setErrors({...errors, nombre:"Hay un error en el email"})
+    }
+    if(form.nombre==="") setErrors({...errors,nombre:"Email vacío"})
+}
+
+const onChangeHandler = (event) => {
+    const property = event.target.name ;    
     const value = event.target.value;
     console.log({[property]:value})
     setForm({...form,[property]: value})
+    validate({...form, [property]:value});
+}
+
+const onSubmitHandler = () => {
+    
 }
     return (
         <form>
             <div>
                 <label>Nombre de la actividad: </label>
-                <input type="text" value={form.nombre} onChange={changeHandler} name="nombre"></input>
+                <input type="text" value={form.nombre} onChange={onChangeHandler} name="nombre"></input>
+                {errors.nombre && <span>{errors.nombre}</span>}
+                
             </div>
 
             <div>
@@ -38,7 +65,8 @@ const changeHandler = (event) => {
                     <option value="valor1">Opción 1</option>
                     <option value="valor2">Opción 2</option>
                     <option value="valor3">Opción 3</option>
-</select>
+                </select>   
+            <button type="submit"></button>
             </div>  
         </form>
     )
